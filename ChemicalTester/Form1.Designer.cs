5ef80e7318bd0e095a8322dabf5783d0dc896d5a
Ustaz1505 <nikma-1@Outlook.com>
2023-03-21 17:45:14 +0300

    Добавьте файлы проекта.

---
+++ ChemicalTester/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ChemicalTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(49, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(599, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Какой режим тестирования вы хотите выбрать?";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("T-FLEX Type B", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(9, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 345);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор режима тестирования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(400, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Паршин Никита 8В на момент 2022-2023";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(54, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(581, 53);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Ещё недописано(";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(46, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(581, 53);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Вопросы по конкретной теме";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(46, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(581, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Автогенерация варианта";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(54, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(581, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Создание варианта";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("T-FLEX Type B", 21.75F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(54, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(581, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Постоянное тестирование";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(3, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(383, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Программа написана с использованием чертежного шрифта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 399);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(706, 438);
+            this.Name = "Form1";
+            this.Text = "Вход в тест по химии";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChemicalTester/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChemicalTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var test = new Test1();
+            test.Location = this.Location;
+            test.StartPosition = FormStartPosition.Manual;
+            test.FormClosing += delegate { this.Show(); };
+            test.Show();
+            this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
